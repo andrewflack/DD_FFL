f32a93d12ec18a848f64046f894ed80a3d8c9518
Andrew Flack <flack.andrew@gmail.com>
2017-09-13 21:07:57 -0400

    Grab and clean 2016 data, update elo ratings

--- src/elo_prep.R
+++ src/elo_prep.R
@@ -1,66 +1,68 @@
 
 # is new season
-results <- results %>% 
+results_w_elo <- results %>% 
   mutate(new_season = ifelse(year != 2010 & week == 1, 1, 0)) %>%
   arrange(year, week)
 
 # is copy
-results.sort <- t(apply(results[,c("year", "week", "owner", "opp_owner")], 1, sort))
-results$is_copy <- ifelse(duplicated(results.sort) == TRUE, 1, 0)
+results_w_elo.sort <- t(apply(results_w_elo[,c("year", "week", "owner", "opp_owner")], 1, sort))
+results_w_elo$is_copy <- ifelse(duplicated(results_w_elo.sort) == TRUE, 1, 0)
 
 # is new owner
 # http://stackoverflow.com/questions/30159041/creating-a-running-counting-variable-in-r
-results$owner_n <- sapply(1:nrow(results), function(i) sum(results$owner[i] == unlist(results[1:i, c("owner")])))
+results_w_elo$owner_n <- sapply(1:nrow(results_w_elo), function(i) sum(results_w_elo$owner[i] == unlist(results_w_elo[1:i, c("owner")])))
 
-results <- results %>%
+results_w_elo <- results_w_elo %>%
   arrange(year, week) %>%
   # new team if owner has only played on game (owner_n = 1) and it's the start of a new season
   mutate(new_owner = ifelse((owner_n == 1 & new_season == 1) | (owner_n == 1 & year == 2010), 1, 0))
 
 # add "period" to count consecutive weeks across full history
-results$period <- rep(1:((length(unique(results$year)))*13), each = 8)
+results_w_elo$period <- rep(1:((length(unique(results_w_elo$year)))*13), each = 8)
 
 # add margin of victory (mov)
-results <- results %>% 
+results_w_elo <- results_w_elo %>% 
   mutate(mov = week_total - opp_week_total)
 
 init <- 1500
 k <- 20
 
 # initialize 3D array to track elo_i and elo_n rating by owner and period
-elohist_i <- array(init, dim = c(length(sort(unique(c(results$owner, results$opp_owner)))), max(results$period) + 1), dimnames = list(sort(unique(results$owner)), seq(1:(max(results$period) + 1))))
-elohist_n <- array(init, dim = c(length(sort(unique(c(results$owner, results$opp_owner)))), max(results$period)), dimnames = list(sort(unique(results$owner)), seq(1:max(results$period))))
+elohist_i <- array(init, dim = c(length(sort(unique(c(results_w_elo$owner, results_w_elo$opp_owner)))), max(results_w_elo$period) + 1), dimnames = list(sort(unique(results_w_elo$owner)), seq(1:(max(results_w_elo$period) + 1))))
+elohist_n <- array(init, dim = c(length(sort(unique(c(results_w_elo$owner, results_w_elo$opp_owner)))), max(results_w_elo$period)), dimnames = list(sort(unique(results_w_elo$owner)), seq(1:max(results_w_elo$period))))
 
-results$elo_i <- NA
-results$elo_n <- NA
-results$opp_elo_i <- NA
-results$elo_diff <- NA
-results$MOVM <- NA
-results$exp <- NA
+results_w_elo$elo_i <- NA
+results_w_elo$elo_n <- NA
+results_w_elo$opp_elo_i <- NA
+results_w_elo$elo_diff <- NA
+results_w_elo$MOVM <- NA
+results_w_elo$exp <- NA
 
-for (i in 1:nrow(results)) {
-  if (results$new_owner[i] == 1) {
-    results$elo_i[i] <- init
+for (i in 1:nrow(results_w_elo)) {
+  if (results_w_elo$new_owner[i] == 1) {
+    results_w_elo$elo_i[i] <- init
   } else {
-    results$elo_i[i] <- elohist_i[results$owner[i], results$period[i]]
+    results_w_elo$elo_i[i] <- elohist_i[results_w_elo$owner[i], results_w_elo$period[i]]
   }
-  results$opp_elo_i[i] <- elohist_i[results$opp_owner[i], results$period[i]]
-  results$elo_diff[i] <- results$elo_i[i] - results$opp_elo_i[i]
-  print(paste0("Owner elo_i: ", results$elo_i[i], " opp_owner elo_i: ", elohist_i[results$opp_owner[i], results$period[i]], " elo_diff: ", results$elo_diff[i]))
-  results$exp[i] <- 1 - 1/((10^(results$elo_diff[i]/400)) + 1)
-  if (results$won_matchup[i] == 1) {
-    results$MOVM[i] <- log(abs(results$mov[i]) + 1)*(2.2/((results$elo_i[i] - results$opp_elo_i[i]) * .001 + 2.2))
+  results_w_elo$opp_elo_i[i] <- elohist_i[results_w_elo$opp_owner[i], results_w_elo$period[i]]
+  results_w_elo$elo_diff[i] <- results_w_elo$elo_i[i] - results_w_elo$opp_elo_i[i]
+  print(paste0("Owner elo_i: ", results_w_elo$elo_i[i], " opp_owner elo_i: ", elohist_i[results_w_elo$opp_owner[i], results_w_elo$period[i]], " elo_diff: ", results_w_elo$elo_diff[i]))
+  results_w_elo$exp[i] <- 1 - 1/((10^(results_w_elo$elo_diff[i]/400)) + 1)
+  if (results_w_elo$won_matchup[i] == 1) {
+    results_w_elo$MOVM[i] <- log(abs(results_w_elo$mov[i]) + 1)*(2.2/((results_w_elo$elo_i[i] - results_w_elo$opp_elo_i[i]) * .001 + 2.2))
   } else {
-    results$MOVM[i] <- log(abs(results$mov[i]) + 1)*(2.2/((results$opp_elo_i[i] - results$elo_i[i]) * .001 + 2.2))
+    results_w_elo$MOVM[i] <- log(abs(results_w_elo$mov[i]) + 1)*(2.2/((results_w_elo$opp_elo_i[i] - results_w_elo$elo_i[i]) * .001 + 2.2))
   }
-  results$elo_n[i] <- results$elo_i[i] + (results$MOVM[i]*k*((abs(results$mov[i])^.8)/(7.5 + .006*(abs(results$elo_diff[i])))))*(results$won_matchup[i]-results$exp[i])
-  elohist_n[results$owner[i], results$period[i]] <- results$elo_n[i]
-  elohist_i[results$owner[i], results$period[i] + 1] <- results$elo_n[i]
+  results_w_elo$elo_n[i] <- results_w_elo$elo_i[i] + (results_w_elo$MOVM[i]*k*((abs(results_w_elo$mov[i])^.8)/(7.5 + .006*(abs(results_w_elo$elo_diff[i])))))*(results_w_elo$won_matchup[i]-results_w_elo$exp[i])
+  elohist_n[results_w_elo$owner[i], results_w_elo$period[i]] <- results_w_elo$elo_n[i]
+  elohist_i[results_w_elo$owner[i], results_w_elo$period[i] + 1] <- results_w_elo$elo_n[i]
 }
 
-results %>% 
+# write.csv(results_w_elo, "data/results_w_elo.csv", row.names = FALSE)
+
+results_w_elo %>% 
   ggplot(aes(x = period, y = elo_n, color = owner)) + 
   facet_wrap(~owner) + 
   geom_line(color = "red") + 
-  geom_vline(xintercept = c(13, 26, 39, 52, 65), color = "grey50") + 
+  geom_vline(xintercept = c(13, 26, 39, 52, 65, 78), color = "grey50") + 
   theme_bw()
